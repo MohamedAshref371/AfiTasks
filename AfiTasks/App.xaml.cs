30f9964cd7c5bd05e72527c2a34252df57b44d46
Mohamed Ashref <AshrefKing523@Gmail.com>
2025-08-24 21:50:00 +0300

    Add project files.

---
+++ AfiTasks/App.xaml.cs
@@ -0,0 +1,105 @@
+﻿using AfiTasks.Properties;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Xml.Linq;
+
+namespace AfiTasks
+{
+    public partial class App : Application
+    {
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private List<string[]> tasks;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            trayIcon = new System.Windows.Forms.NotifyIcon
+            {
+                Icon = AfiTasks.Properties.Resources.board,
+                Visible = true,
+                Text = "AfiTasks"
+            };
+
+            string[] arr = System.IO.File.ReadAllLines("Tasks.txt").Select(s => s.Trim()).Where(s => s != "").ToArray();
+            tasks = new List<string[]>();
+            List<string> current = new List<string>();
+
+            foreach (var item in arr)
+            {
+                if (item.Contains("***"))
+                {
+                    if (current.Count > 0) tasks.Add(current.ToArray());
+                    current.Clear();
+                }
+                else
+                    current.Add(item);
+            }
+
+            if (current.Count > 0) tasks.Add(current.ToArray());
+
+            //var tasksMenu = new System.Windows.Forms.ToolStripMenuItem("قائمة المهام");
+            //for (int i = 0; i < tasks.Count; i++)
+            //{
+            //    int idx = i;
+            //    tasksMenu.DropDownItems.Add(tasks[i][0], null, (s, ev) => RunTask(idx));
+            //}
+
+            var menu = new System.Windows.Forms.ContextMenuStrip();
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                int idx = i;
+                menu.Items.Add(tasks[i][0], null, (s, ev) => RunTask(idx));
+            }
+            menu.Items.Add("خروج", null, (s, ev) => ExitApp());
+            trayIcon.ContextMenuStrip = menu;
+
+            // دبل كليك على الأيقونة
+            //trayIcon.DoubleClick += (s, ev) => ShowMainWindow();
+
+            // افتح النافذة الأساسية أول مرة
+            //ShowMainWindow();
+        }
+
+        private void RunTask(int i)
+        {
+            MainWindow mw = new MainWindow(tasks[i], i % 2);
+            mw.Show();
+        }
+
+        //private void ShowMainWindow()
+        //{
+        //    if (Current.MainWindow == null)
+        //    {
+        //        Current.MainWindow = new MainWindow();
+        //        Current.MainWindow.Closed += (s, e) => Current.MainWindow = null;
+        //    }
+
+        //    if (!Current.MainWindow.IsVisible)
+        //        Current.MainWindow.Show();
+
+        //    if (Current.MainWindow.WindowState == WindowState.Minimized)
+        //        Current.MainWindow.WindowState = WindowState.Normal;
+
+        //    Current.MainWindow.Activate();
+        //}
+
+        private void ExitApp()
+        {
+            trayIcon.Visible = false;
+            trayIcon.Dispose();
+            Current.Shutdown();
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            trayIcon.Visible = false;
+            trayIcon.Dispose();
+        }
+    }
+}
---
+++ AfiTasks/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AfiTasks
+{
+    public partial class MainWindow : Window
+    {
+        int idx; string[] elements;
+        int colorState;
+        public MainWindow(string[] elements, int colorState)
+        {
+            InitializeComponent();
+            this.elements = elements;
+            WindowCounter.Text = $"{idx}/{elements.Length - 1}";
+            WindowText.Text = elements[idx];
+            this.colorState = colorState;
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            double oldWidth = this.Width, oldHeight = this.Height;
+            WindowText.FontSize = 42;
+
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                this.Left += (oldWidth - this.Width) / 2;
+                this.Top += (oldHeight - this.Height) / 2;
+            }), System.Windows.Threading.DispatcherPriority.Render);
+
+            if (colorState == 1)
+            {
+                TextColorOne.Color = Color.FromRgb(0, 0, 255);
+                TextColorTwo.Color = Color.FromRgb(0, 165, 255);
+                TextColorThree.Color = Color.FromRgb(0, 255, 255);
+                return;
+            }
+            else
+            {
+                TextColorOne.Color = Color.FromRgb(255, 0, 0);
+                TextColorTwo.Color = Color.FromRgb(255, 165, 0);
+                TextColorThree.Color = Color.FromRgb(255, 255, 0);
+            }
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            double oldWidth = this.Width, oldHeight = this.Height;
+            WindowText.FontSize = 35;
+
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                this.Left += (oldWidth - this.Width) / 2;
+                this.Top += (oldHeight - this.Height) / 2;
+            }), System.Windows.Threading.DispatcherPriority.Render);
+
+            TextColorOne.Color = Color.FromRgb(100, 100, 100);
+            TextColorTwo.Color = Color.FromRgb(150, 150, 150);
+            TextColorThree.Color = Color.FromRgb(200, 200, 200);
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Right && idx < elements.Length - 1)
+                idx += 1;
+            else if (e.ChangedButton == MouseButton.Middle && idx > 0)
+                idx -= 1;
+            
+            WindowText.Text = elements[idx];
+            WindowCounter.Text = $"{idx}/{elements.Length - 1}";
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9 && WindowNumber.Text.Length < 6)
+            {
+                int number = e.Key - Key.NumPad0;
+                WindowNumber.Text += number.ToString();
+            }
+            else if (e.Key == Key.Escape)
+                this.Hide();
+        }
+    }
+}
